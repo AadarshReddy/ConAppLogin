1c46c395e6c63f00ca4c40cdbe9d833f246e6b85
Aadarsh Reddy <areddy5757@gmail.com>
2024-02-27 22:03:55 +0530

    Add project files.

---
+++ ConAppLogin/Authentication.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppLogin
+{
+    public class Authentication
+    {
+        public  string Login(string username, string password)
+        {
+            string loginMsg;
+            if(string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                loginMsg = "please provide credentials";
+
+            }
+            else
+            {
+                if(((username == "admin") && (password == "admin@123")) || ((username == "sam")&&  (password == "sam@123")))
+                {
+                    loginMsg = "login Success";
+                }
+                else
+                {
+                    loginMsg = "login failed";
+                }
+                
+            }
+            return loginMsg;
+        }
+    }
+}
---
+++ ConAppLogin/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConAppLogin;
+
+Console.WriteLine("Bank Login");
+
+try
+{
+    string userName, password;
+    Console.WriteLine("Enter UserName");
+    userName = Console.ReadLine();
+    Console.WriteLine("Enter PAssword");
+    password = Console.ReadLine();
+    Authentication auth = new Authentication();
+    Console.WriteLine(auth.Login(userName,password));
+}
+catch(Exception ex)
+{ Console.WriteLine("Error!!!!"+ex.Message); }
